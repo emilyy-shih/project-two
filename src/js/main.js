789be12d64adada0f7318c3731eac155ce4c87b8
EMILY SHIH <emilyyshih@gmail.com>
2021-11-01 13:50:29 -0400

    accordion and footer

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,10 @@
+var accordions = document.querySelectorAll(".accordion");
+var i;
 
+for (i = 0; i < accordions.length; i++) {
+  accordions[i].addEventListener("click", function () {
+    /* Toggle between adding and removing the "active" class,
+    to highlight the button that controls the panel */
+    this.classList.toggle("active");
+  });
+}
